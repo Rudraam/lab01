35aff45646c990918f9e34c79d6e9020d83b88a2
Your Name <your-email@example.com>
2025-01-10 15:26:07 -0500

    first lab done

---
+++ week1/lab01.py
@@ -0,0 +1,17 @@
+# Sample Coding Questions 01 Week 01
+# Rudramani
+# Dhiman
+num = [1,4,7,9]
+a=1
+b=2
+c=3
+d=4
+e = (a * c) - (b / d)
+print(e)
+###3############################################
+temperature= 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Enter Age: "))
+userAge += 22
+print("Items filtered by age: ", userAge)
